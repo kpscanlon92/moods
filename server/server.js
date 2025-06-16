a1dfbc7a738f9679e4e104023ed1e277d352b7a3
Kelly Scanlon <kpscanlon92@gmail.com>
2025-06-16 15:43:17 -0700

    Handle SPA on render in settings to keep frontend and backend deployed separately

--- server/server.js
+++ server/server.js
@@ -1,7 +1,4 @@
-import express from 'express';
 import mongoose from 'mongoose';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 import app from './app.js';
 
@@ -9,17 +6,6 @@ import app from './app.js';
 const uri = process.env.MONGO_URI;
 await mongoose.connect(uri);
 
-// Serve static files (only in production)
-if (process.env.NODE_ENV === "production") {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    app.use(express.static(path.join(__dirname, '../client/dist')));
-
-    // Catch-all for SPA routing
-    app.get('/{*any}', (req, res) => {
-        res.sendFile(path.join(__dirname, '../client', 'dist', 'index.html'));
-    });
-}
 
 const port = 4000;
 app.listen(port, () => {
